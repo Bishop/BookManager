a827a719d5543d0139e984e00adbdcba8eaf204b
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-07-24 00:53:18 +0300

    Add created_at and updated_at fields to book model, change migration

--- BookManager/migrations/0002_auto__add_book.py
+++ BookManager/migrations/0002_auto__add_book.py
@@ -11,6 +11,8 @@ class Migration(SchemaMigration):
         # Adding model 'Book'
         db.create_table('BookManager_book', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
             ('title', self.gf('django.db.models.fields.TextField')()),
             ('author', self.gf('django.db.models.fields.TextField')()),
             ('year', self.gf('django.db.models.fields.DateField')()),
@@ -31,11 +33,13 @@ class Migration(SchemaMigration):
         'BookManager.book': {
             'Meta': {'object_name': 'Book'},
             'author': ('django.db.models.fields.TextField', [], {}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'edition': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'pages': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
             'publisher': ('django.db.models.fields.CharField', [], {'max_length': '150', 'null': 'True', 'blank': 'True'}),
             'title': ('django.db.models.fields.TextField', [], {}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'year': ('django.db.models.fields.DateField', [], {})
         }
     }
--- BookManager/models.py
+++ BookManager/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 nullable = {'null': True, 'blank': True}
 
-class Book(models.Model):
+class ProtocolModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Book(ProtocolModel):
     title = models.TextField()
     author = models.TextField()
     year = models.DateField()
     edition = models.CharField(max_length=50, **nullable)
     publisher = models.CharField(max_length=150, **nullable)
     pages = models.IntegerField(**nullable)
-    
