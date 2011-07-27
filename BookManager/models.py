3e5a51ff6b3ff4f887c18931fc88076318054c72
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-07-27 22:54:55 +0300

    Added databrowse

--- BookManager/models.py
+++ BookManager/models.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.forms.models import ModelForm
+from django.forms import HiddenInput
 
 nullable = {'null': True, 'blank': True}
 
@@ -20,14 +21,17 @@ class Book(ProtocolModel):
     def get_absolute_url(self):
         return reverse('book_detail', args=[self.pk])
 
+    def __unicode__(self):
+        return u"{0} ({1}) [{2}]".format(self.title, self.author, self.year)
+
 class File(ProtocolModel):
     book = models.ForeignKey(Book)
-    path = models.URLField()
+    path = models.URLField(verify_exists=False)
     note = models.TextField(**nullable)
 
 class Publisher(ProtocolModel):
     title = models.CharField(max_length=100, unique=True)
-    url = models.URLField(unique=True, **nullable)
+    url = models.URLField(unique=True, verify_exists=False, **nullable)
 
 class Edition(ProtocolModel):
     isbn = models.CharField(max_length=20)
@@ -37,3 +41,10 @@ class Edition(ProtocolModel):
 class BookForm(ModelForm):
     class Meta:
         model = Book
+
+class FileForm(ModelForm):
+    class Meta:
+        model = File
+        widgets = {
+            'book': HiddenInput(),
+        }
--- urls.py
+++ urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
+from django.contrib import databrowse
+from BookManager.models import File, Publisher, Book, Edition
 
 admin.autodiscover()
+
+for model in [Book, Publisher, File, Edition]:
+    databrowse.site.register(model)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'books.views.home', name='home'),
     url(r'^books/', include('BookManager.urls')),
+    url(r'^databrowse/(.*)', databrowse.site.root)
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
