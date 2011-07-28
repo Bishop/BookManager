310b3d4355eb6da78666980335172a4cfb24f9d6
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-07-29 00:12:56 +0300

    Display 'add file' form

--- BookManager/views.py
+++ BookManager/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
-from BookManager.models import Book
+from BookManager.models import Book, FileForm, File
 
 class BookListView(ListView):
     paginate_by = 10
@@ -20,3 +20,11 @@ class BookDetail(DetailView):
     model = Book
     template_name = 'book_detail.html'
     context_object_name = 'book'
+
+    def render_to_response(self, context, **response_kwargs):
+        context['file_form'] = FileForm(initial={'book': context['object']})
+        return super(BookDetail, self).render_to_response(context, **response_kwargs)
+
+    def get(self, request, **kwargs):
+        return super(BookDetail, self).get(request, **kwargs)
+
