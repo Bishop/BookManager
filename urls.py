eb1f4d284d0d5d13793f3d02c84b52b01f1a2e79
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-08-15 23:54:18 +0300

    Main menu and home page

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
 from django.contrib import databrowse
+from django.views.generic.base import TemplateView
 from BookManager.models import File, Publisher, Book, Edition
 
 admin.autodiscover()
@@ -10,6 +11,7 @@ for model in [Book, Publisher, File, Edition]:
     databrowse.site.register(model)
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home_page'),
     url(r'^books/', include('BookManager.urls')),
     url(r'^databrowse/(.*)', databrowse.site.root),
     url(r'^admin/', include(admin.site.urls)),
