a0b73b541df2c9eddb8620128a36a1fa65c8e19b
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-08-18 00:41:24 +0300

    Added empty user_detail page

--- account/views.py
+++ account/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.views.generic.detail import DetailView
 
 class Account(DetailView):
-    pass
+    def get_object(self, queryset=None):
+        return self.request.user
