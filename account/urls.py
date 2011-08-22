95ba640a6af009632e11d6a76dad1c21ce2e1c00
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-08-22 23:04:17 +0300

    Simple profile page

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ from account.views import *
 urlpatterns = patterns('',
     url(r'^$', login_required(Account.as_view()), name='account_summary'),
     url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
 )
